d1faf8d11580daaa7acecbd3d2468e2cafb718d6
Ian Kropp <iankropp@gmail.com>
2016-12-24 15:25:20 -0500

    Implemented simple switch

--- main.c
+++ main.c
@@ -1,39 +1,44 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define LED1 PIND0
+#define LED2 PIND1
+#define LED3 PIND2
+#define LED4 PIND3
+#define LED5 PIND4
+
+#define FLOOR_SWITCH PINB0
+
 int main()
 {
-  DDRD |= 1 << PIND0;
+
+  // turning on the output leds
+  DDRD |= 1 << LED1;
+  DDRD |= 1 << LED2;
+  DDRD |= 1 << LED3;
+  DDRD |= 1 << LED4;
+  DDRD |= 1 << LED5;
+
+  // turning the floor switch to input
+  DDRB &= ~(1 << FLOOR_SWITCH);
+
+  PORTB |= (1 << FLOOR_SWITCH);
 
   while(1)
   {
-    PORTD |= (1 << PIND0);
-    _delay_ms(100);
-    PORTD &= ~(1 << PIND0);
-    _delay_ms(100);
 
-    PORTD |= (1 << PIND1);
-    _delay_ms(100);
-    PORTD &= ~(1 << PIND1);
-    _delay_ms(100);
+    if(bit_is_clear(PINB, FLOOR_SWITCH)){
+    
+      PORTD |= (1 << LED1) | (1 << LED2) | (1 << LED3) | (1 << LED4) | (1 << LED4) | (1 << LED5);
 
-    PORTD |= (1 << PIND2);
-    _delay_ms(100);
-    PORTD &= ~(1 << PIND2);
-    _delay_ms(100);
+    } else {
+    
+      PORTD &= ~((1 << LED1) | (1 << LED2) | (1 << LED3) | (1 << LED4) | (1 << LED4)  | (1 << LED5));
 
-    PORTD |= (1 << PIND3);
-    _delay_ms(100);
-    PORTD &= ~(1 << PIND3);
-    _delay_ms(100);
-
-    /*PORTD |= (1 << PIND4);
-    _delay_ms(100);
-    PORTD &= ~(1 << PIND4);
-    _delay_ms(100);*/
-
+    }
 
   }
+
 }
 
 
